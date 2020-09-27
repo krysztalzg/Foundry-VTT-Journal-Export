456acf0b5f800adbb2a87bd2db1459fa10578973
Kamil Szczepański <kamil.szczepanski@netguru.com>
2020-09-27 23:18:17 +0200

    Add navigation files

--- journal_export.py
+++ journal_export.py
@@ -20,9 +20,10 @@ def run_export(pathToWorldDirectory):
     print(f'Running Journal Exporter for {pathToWorldDirectory}')
     directories = load_directories_hierarchy(pathToWorldDirectory)
     entries = load_journal_entries(pathToWorldDirectory)
+    directories = [d for d in directories if d.id in [e.directory for e in entries] + [d.parent for d in directories]]
     create_journal_folders(directories, entries, pathToWorldDirectory)
     create_journal_files(entries, directories)
-    create_index_file(directories, entries)
+    create_index_files(directories, entries, pathToWorldDirectory)
     copy_image_files(entries, pathToWorldDirectory)
 
 # DB file readers.
@@ -30,7 +31,7 @@ def run_export(pathToWorldDirectory):
 def load_directories_hierarchy(pathToWorldDirectory):
     print('Loading Journal Directories from `folders.db`...')
     foldersdb_path = pathToWorldDirectory + safe_path('\\data\\folders.db')
-    with open(foldersdb_path, 'r', encoding="utf8") as f:
+    with open(foldersdb_path, 'r', encoding='utf8') as f:
         directories = [parse_folder_entry(line) for line in f if line]
     print('Journal Directories loaded.')
     return [directory for directory in directories if directory]
@@ -38,7 +39,7 @@ def load_directories_hierarchy(pathToWorldDirectory):
 def load_journal_entries(pathToWorldDirectory):
     print('Loading Journal Entries from `journal.db`.')
     journaldb_path = pathToWorldDirectory + safe_path('\\data\\journal.db')
-    with open(journaldb_path, 'r', encoding="utf8") as f:
+    with open(journaldb_path, 'r', encoding='utf8') as f:
         entries = [parse_journal_entry(line) for line in f]
     print('Journal Entries loaded.')
     return [entry for entry in  entries if entry]
@@ -76,7 +77,6 @@ def parse_journal_entry(line):
 # Markdown exporters.
 
 def create_journal_folders(directories, entries, pathToWorldDirectory):
-    print(f'World name: {pathToWorldDirectory.split(os.path.sep)[-1]}')
     root_folder_path = '{}{}{}'.format('Journal Export', os.path.sep, pathToWorldDirectory.split(os.path.sep)[-1])
     print(f'Creating root export directory `{root_folder_path}`... ', end='')
     try:
@@ -86,7 +86,7 @@ def create_journal_folders(directories, entries, pathToWorldDirectory):
         print(f'Failed | {sys.exc_info()}')
         sys.exit(2)
     [create_directory_path(d, directories, root_folder_path) for d in directories]
-    for path in [d.path for d in directories if d.id in [e.directory for e in entries]]:
+    for path in [d.path for d in directories]:
         try:
             print(f'Creating directory `{path}`... ', end='')
             os.makedirs(path, exist_ok=True)
@@ -102,15 +102,31 @@ def create_journal_files(entries, directories):
         entry.path = '{}{}{}.md'.format(directory.path, os.path.sep, entry.name)
         try:
             print(f'Creating Journal entry `{entry.path}`... ', end='')
-            with open(entry.path, 'w', encoding="utf8") as f:
+            with open(entry.path, 'w', encoding='utf8') as f:
                 f.write(entry.content)
             print('Created')
         except:
             print(f'Failed | {sys.exc_info()}')
             continue
 
-def create_index_file(directories, entries):
-    pass
+def create_index_files(directories, entries, pathToWorldDirectory):
+    print('Creating navigation files.')
+    created_readme_files = []
+    root_folder_path = '{}{}{}'.format('Journal Export', os.path.sep, pathToWorldDirectory.split(os.path.sep)[-1])
+    file_paths = [d.path.replace(root_folder_path, '') for d in directories] + [e.path.replace(root_folder_path, '') for e in entries]
+    for file_path in file_paths:
+        path_fragments = file_path.split(os.path.sep)
+        file_path_without_name = os.path.sep.join(path_fragments[:-1])
+        readme_path = '{}{}{}README.md'.format(root_folder_path, file_path_without_name, os.path.sep)
+        print(f'Creating link for `{file_path_without_name}`... ', end='')
+        if readme_path not in created_readme_files:
+            with open(readme_path, 'w', encoding='utf8') as f:
+                f.write(f'Journal Entries for `{pathToWorldDirectory.split(os.path.sep)[-1]}{file_path_without_name}`:\n')
+            created_readme_files.append(readme_path)
+        with open(readme_path, 'a', encoding='utf8') as f:
+            file_name = path_fragments[-1]
+            f.write(f'- [{file_name.replace(".md", "")}]({parse.quote(file_name)})\n')
+        print('Created')
 
 def copy_image_files(entries, pathToWorldDirectory):
     path_to_data = re.search(r'(.*Data).*', pathToWorldDirectory).groups()[0]
@@ -154,6 +170,7 @@ def safe_path(path):
 
 def main():
     print('Journal Export by Thandulfan')
+    pathToWorldDirectory = ''
     try:
         opts, args = getopt.getopt(sys.argv[1:] ,'help:h', ['world=',])
     except:
